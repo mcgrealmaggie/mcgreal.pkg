75a1e291b4b03d625575819f49cc90287ab2243f
Maggie McGreal <maggie.mcgreal@selu.edu>
2023-09-26 18:58:19 +0000

    Selecting x, y, and z columns and adding remove NAs function to package

--- mcgreal.pkg/R/remove_na.R
+++ mcgreal.pkg/R/remove_na.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 
-remove_na <- function(acc_x, acc_y, acc_z) {
-  accx <- filter()
+remove_na <- function(x, y, z) {
+  na <- data %>% 
+    select(x, y, z) %>% 
+    na.omit()
+  return(na)
 }
